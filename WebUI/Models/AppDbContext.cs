c6692b941d107aa0c41d1de0a7ba463079c70a4c
Murad <murad.yunus.2017@gmail.com>
2023-09-21 18:35:32 +0400

    Add first migration

--- WebUI/Models/AppDbContext.cs
+++ WebUI/Models/AppDbContext.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebUI.Models
 {
-    public class AppDbContext : IdentityDbContext
+    public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
