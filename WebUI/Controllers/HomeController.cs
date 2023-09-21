185448e352dc817a085a807681b0f3d2aef05509
Murad <murad.yunus.2017@gmail.com>
2023-09-21 20:43:29 +0400

    Register user

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using WebUI.Models;
+using WebUI.ViewModels;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly UserManager<AppUser> userManager;
+        public HomeController(ILogger<HomeController> logger,
+                              UserManager<AppUser> userManager)
         {
             _logger = logger;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
@@ -20,6 +24,43 @@ namespace WebUI.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpViewModel request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            AppUser user = new()
+            {
+                UserName = request.UserName,
+                Email = request.Email,
+                PhoneNumber = request.Phone
+            };
+            var identityResult = await userManager.CreateAsync(user, request.Password);
+
+            if (identityResult.Succeeded)
+            {
+                TempData["SuccessedMessage"] = "İstifadəçi uğurla yaradıldı.";
+
+                return RedirectToAction(nameof(HomeController.SignUp));
+            }
+
+            foreach (var item in identityResult.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
             return View();
         }
 
---
+++ WebUI/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.ViewModels
+{
+    public class SignUpViewModel
+    {
+        [Display(Name = "Istifadəçi adı: ")]
+        [Required(ErrorMessage = "Istifadəçi adı boş ola bilməz")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "Email: ")]
+        [Required(ErrorMessage = "Email boş ola bilməz")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlışdır")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Telefon: ")]
+        [Required(ErrorMessage = "Telefon ola bilməz")]
+        public string Phone { get; set; }
+
+
+        [Display(Name = "Parol: ")]
+        [Required(ErrorMessage = "Parol boş ola bilməz")]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Təkrar parol: ")]
+        [Required(ErrorMessage = "Təkrar parol boş ola bilməz")]
+        [Compare(nameof(Password), ErrorMessage = "Parol eyni deyildir")]
+        public string PasswordConfirm { get; set; }
+    }
+}
