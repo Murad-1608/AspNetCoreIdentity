a8101b9a7c6105242a51863291089ebf2f8522a1
Murad <murad.yunus.2017@gmail.com>
2023-09-22 08:51:00 +0400

    Client-side validation in SignUp

---
+++ WebUI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebUI.Areas.Admin.Models;
+using WebUI.Models;
+
+namespace WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UserList()
+        {
+            var userList = await userManager.Users.ToListAsync();
+
+            var userViewModelList = userList.Select(x => new UserViewModel()
+            {
+                Id = x.Id,
+                Name = x.UserName,
+                Email = x.Email
+            }).ToList();
+
+            return View(userViewModelList);
+        }
+    }
+}
---
+++ WebUI/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUI.Areas.Admin.Models
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
