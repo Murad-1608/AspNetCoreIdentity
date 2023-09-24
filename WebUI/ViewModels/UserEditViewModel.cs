d85f89becac755351993a1335fcd794c629a31bb
Murad <murad.yunus.2017@gmail.com>
2023-09-24 13:17:44 +0400

    Update appUser table

---
+++ WebUI/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.ViewModels
+{
+    public class UserEditViewModel
+    {
+        [Display(Name = "Istifadəçi adı: ")]
+        [Required(ErrorMessage = "Istifadəçi adı boş ola bilməz")]
+        public string UserName { get; set; }
+
+
+        [Display(Name = "Email: ")]
+        [Required(ErrorMessage = "Email boş ola bilməz")]
+        [EmailAddress(ErrorMessage = "Email formatı yanlışdır")]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Telefon: ")]
+        [Required(ErrorMessage = "Telefon ola bilməz")]
+        public string Phone { get; set; }
+    }
+}
