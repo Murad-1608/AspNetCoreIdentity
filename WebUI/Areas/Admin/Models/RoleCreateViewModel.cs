30d32d1f347a0aafeab8e308a3f098563dc8f2cf
Murad <murad.yunus.2017@gmail.com>
2023-09-28 23:52:16 +0400

    Configuration add role page

--- WebUI/Areas/Admin/Models/RoleCreateViewModel.cs
+++ WebUI/Areas/Admin/Models/RoleCreateViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace WebUI.Areas.Admin.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Areas.Admin.Models
 {
     public class RoleCreateViewModel
     {
+        [Display(Name = "Rol")]
+        [Required(ErrorMessage = "Rol boş ola bilməz")]
         public string Name { get; set; }
     }
 }
