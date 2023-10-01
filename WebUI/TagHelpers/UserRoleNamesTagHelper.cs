7e2b812bb903cd78c01e5afd78d635fbecc771a4
Murad <murad.yunus.2017@gmail.com>
2023-10-01 23:23:21 +0400

    Show users role in userlist

---
+++ WebUI/TagHelpers/UserRoleNamesTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Text;
+using WebUI.Models;
+
+namespace WebUI.TagHelpers
+{
+    public class UserRoleNamesTagHelper : TagHelper
+    {
+        public string UserId { get; set; }
+        private readonly UserManager<AppUser> userManager;
+        public UserRoleNamesTagHelper(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var user = await userManager.FindByIdAsync(UserId);
+
+            var userRoles = await userManager.GetRolesAsync(user);
+
+            StringBuilder stringBuilder = new();
+
+            userRoles.ToList().ForEach(x =>
+            {
+                stringBuilder.Append(@$"<span class='badge bg-secondary'>{x.ToLower()}</span>");
+            });
+
+            output.Content.SetHtmlContent(stringBuilder.ToString());
+        }
+    }
+}
